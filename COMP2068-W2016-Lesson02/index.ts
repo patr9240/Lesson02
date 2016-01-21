f292dbb1e350aa256aff36fbc8da03d2bceb1a5b
Patrick Ross <patr9240@gmail.com>
2016-01-21 14:51:51 -0500

    initial commit

---
+++ COMP2068-W2016-Lesson02/index.ts
@@ -0,0 +1,23 @@
+import http = require('http');
+
+
+var hostname = '127.0.0.1';
+var port = 8080;
+
+
+http.createServer(function(req:http.ServerRequest, res:http.ServerResponse){
+    res.writeHead(200, { 'Content-Type' : 'text/plain'});
+    res.end("Hello World\n");
+}).listen(port, hostname, function(){
+    console.log("Server running at http://" + hostname + ":" + port);
+});
+
+
+/*
+http.createServer(function(req, res)  {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port, hostname, function() {
+    console.log("Server running at http://" + hostname + ":" + port );
+});
+*/
