3932560552ae3a5bc597cd277d9ca5bdad3028d5
Patrick Ross <patr9240@gmail.com>
2016-01-21 15:15:25 -0500

    Revert "Revert "second commit""
    This reverts commit d99a0dfaf3db3776ab27d62158b69675ac1eb9c5.

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var http = require('http');
-var hostname = '127.0.0.1';
-var port = 8080;
+var port = process.env.PORT || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end("Hello World\n");
-}).listen(port, hostname, function () {
-    console.log("Server running at http://" + hostname + ":" + port);
-});
+}).listen(port);
 /*
 http.createServer(function(req, res)  {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
--- index.ts
+++ index.ts
@@ -1,18 +1,10 @@
 import http = require('http');
+var port = process.env.PORT || 8080;
 
-
-var hostname = '127.0.0.1';
-var port = 8080;
-
-
-http.createServer(function(req:http.ServerRequest, res:http.ServerResponse){
-    res.writeHead(200, { 'Content-Type' : 'text/plain'});
+http.createServer(function(req: http.ServerRequest, res: http.ServerResponse) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end("Hello World\n");
-}).listen(port, hostname, function(){
-    console.log("Server running at http://" + hostname + ":" + port);
-});
-
-
+}).listen(port);
 /*
 http.createServer(function(req, res)  {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
